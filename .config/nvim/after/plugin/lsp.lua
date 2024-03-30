df63aea533eef0f42e9a5299214134ba1d91217e
Linus <linus.witte@proton.me>
2024-03-30 19:29:23 +0100

    added stow-ignore and an install script

--- .config/nvim/after/plugin/lsp.lua
+++ .config/nvim/after/plugin/lsp.lua
@@ -10,7 +10,7 @@ require('mason').setup({})
 require('mason-lspconfig').setup({
 	-- Replace the language servers listed here 
 	-- with the ones you want to install
-	ensure_installed = {'lua_ls', 'clangd'},
+	ensure_installed = {'lua_ls', 'clangd', 'bashls'},
 	handlers = {
 		lsp_zero.default_setup,
 	},
---
+++ install.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+~/.tmux/plugins/tpm/scripts/install_plugins.sh
