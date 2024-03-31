6f46b04ffca0155e047d8921a92512573472cbf1
Linus <linus.witte@proton.me>
2024-03-31 23:55:49 +0000

    installscript update

--- install.sh
+++ install.sh
@@ -1,3 +1,12 @@
 #!/bin/bash
+
+# install dependencies
+yay -S nvim nvim-packer-git tmux
+
+# link files 
+stow --adopt .
+
+# setup tmux
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+# sync tmux packages
 ~/.tmux/plugins/tpm/scripts/install_plugins.sh
