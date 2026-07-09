875370d2e614ede300d7b226d88bbf52cb8c25d9
Linus Witte <linus.witte@proton.me>
2026-07-09 15:30:22 +0200

    Switch NixOS bootloader to GRUB

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -97,7 +97,10 @@ in
     ./hyprland.nix
   ];
 
-  boot.loader.systemd-boot.enable = true;
+  boot.loader.grub.enable = true;
+  boot.loader.grub.efiSupport = true;
+  boot.loader.grub.device = "nodev";
+  boot.loader.grub.useOSProber = true;
   boot.loader.efi.canTouchEfiVariables = true;
 
   nixpkgs.config.allowUnfree = true;
