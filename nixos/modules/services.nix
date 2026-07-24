d03a8ac8525c8ad7d0368c009d925b538e0d2548
Linus Witte <linus.witte@proton.me>
2026-07-24 10:47:43 +0200

    Enable NVIDIA CDI for containers

--- nixos/modules/services.nix
+++ nixos/modules/services.nix
@@ -2,8 +2,14 @@
 
 {
   # Membership in the docker group is root-equivalent.
-  virtualisation.docker.enable = true;
+  virtualisation.docker = {
+    enable = true;
+    daemon.settings.features.cdi = true;
+  };
   virtualisation.podman.enable = true;
+
+  # Generate CDI device specifications for NVIDIA-enabled containers.
+  hardware.nvidia-container-toolkit.enable = true;
 
   services.openssh = {
     enable = true;
